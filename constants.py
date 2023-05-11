ebf2028a853b0f975f30660553c214b774b0d371
GodhuliDe <gde@nyit.edu>
2023-05-11 19:53:06 -0400

    First Commit

---
+++ constants.py
@@ -0,0 +1,11 @@
+'''Author History- Created by Godhuli'''
+
+TOPIC = "Mental Health";
+model="davinci"
+model1="text-davinci-003"
+model2="gpt-3.5-turbo"
+
+
+API_KEY="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"
+welcome = "Welcome to HealthGenie.AI - Your Mental Health Assistant"
+intro='I am HealthGenie, I am an mental health assistant powered by OpenAI. \n How may I help you today?'
